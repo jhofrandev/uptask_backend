eeb2a9b3e7272ba5cd26b2247316fe801642cfcd
jhofrandev <jhofran.dev@outlook.com>
2025-08-04 11:39:16 -0500

    feat: add CORS configuration and integrate it into the server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import { connectDB } from "./config/db";
+import { corsConfig } from "./config/cors";
 import projectRoutes from "./routes/projectRoutes";
 
 dotenv.config();
+
 connectDB();
+
 const app = express();
+
+app.use(cors(corsConfig));
+
 app.use(express.json());
 
 // Routes
