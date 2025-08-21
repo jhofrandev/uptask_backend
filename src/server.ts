c86153229f048b251571863b8db329eb72c27d88
jhofrandev <jhofran.dev@outlook.com>
2025-08-21 16:15:22 -0500

    feat: add authentication routes and update CORS configuration

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import express from "express";
 import { connectDB } from "./config/db";
 import { corsConfig } from "./config/cors";
+import authRoutes from "./routes/authRoutes";
 import projectRoutes from "./routes/projectRoutes";
 
 dotenv.config();
@@ -19,6 +20,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 // Routes
+app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 
 export default app;
