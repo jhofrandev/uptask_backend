b523e90eee6592899e644565d537edb0bcf12f2b
jhofrandev <jhofran.dev@outlook.com>
2025-08-22 15:47:04 -0500

    feat: implement create account route with validation and controller integration

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,25 @@
 import { Router } from "express";
+import { AuthController } from "../controllers/AuthController";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Auth route is working");
-});
+router.post(
+  "/create-account",
+  body("name").notEmpty().withMessage("El nombre no puede ir vacio"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("El password es muy corto minimo 8 caracteres"),
+  body("password_confirmation").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Los passwords no son iguales");
+    }
+    return true;
+  }),
+  body("email").isEmail().withMessage("E-mail no valido"),
+  handleInputErrors,
+  AuthController.createAccount
+);
 
 export default router;
