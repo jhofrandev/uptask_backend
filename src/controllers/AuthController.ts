c494d31e6099cfabf040cd9237387bc7b2c3ed26
jhofrandev <jhofran.dev@outlook.com>
2025-08-28 15:57:14 -0500

    feat(auth): implement login functionality with password verification and token handling

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,6 @@
 import User from "../models/User";
 import type { Request, Response } from "express";
-import { hashPassword } from "../utils/auth";
+import { checkPassword, hashPassword } from "../utils/auth";
 import { generateToken } from "../utils/token";
 import Token from "../models/Token";
 import { AuthEmail } from "../emails/AuthEmail";
@@ -50,7 +50,7 @@ export class AuthController {
       const tokenExists = await Token.findOne({ token });
       if (!tokenExists) {
         const error = new Error("Token no valido");
-        return res.status(401).json({ error: error.message });
+        return res.status(404).json({ error: error.message });
       }
 
       const user = await User.findById(tokenExists.user);
@@ -58,6 +58,45 @@ export class AuthController {
 
       await Promise.allSettled([user.save(), tokenExists.deleteOne()]);
       res.send("Cuanta confirmada correctamente");
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un error" });
+    }
+  };
+
+  static login = async (req: Request, res: Response) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user) {
+        const error = new Error("Usuario no existe");
+        return res.status(404).json({ error: error.message });
+      }
+
+      if (!user.confirmed) {
+        const token = new Token();
+        token.user = user.id;
+        token.token = generateToken();
+        await token.save();
+
+        AuthEmail.sendConfirmationEmail({
+          email: user.email,
+          name: user.name,
+          token: token.token,
+        });
+
+        const error = new Error(
+          "Tu cuenta no ha sido confirmada, te hemos reeviado un nuevo email"
+        );
+        return res.status(401).json({ error: error.message });
+      }
+
+      const isPasswordCorrect = await checkPassword(password, user.password);
+      if (!isPasswordCorrect) {
+        const error = new Error("Password incorrecto");
+        return res.status(401).json({ error: error.message });
+      }
+
+      res.send("Login correcto");
     } catch (error) {
       res.status(500).json({ error: "Hubo un error" });
     }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -29,4 +29,12 @@ router.post(
   AuthController.confirmAccount
 );
 
+router.post(
+  "/login",
+  body("email").isEmail().withMessage("E-mail no valido"),
+  body("password").notEmpty().withMessage("El password no puede ir vacio"),
+  handleInputErrors,
+  AuthController.login
+);
+
 export default router;
